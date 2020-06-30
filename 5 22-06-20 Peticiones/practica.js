d6c8251a4ab7ac93bc8d7928651d1bd16b1fe948
yosef sisero <yosefsisero@gmail.com>
2020-06-30 12:53:32 -0300

    Peticiones

--- 5 22-06-20 Peticiones/practica.js
+++ 5 22-06-20 Peticiones/practica.js
@@ -73,13 +73,13 @@ request('http://openlibrary.org/search.json?q=i+robot', function(error, response
 
     const request = require("request");
 
-    const POKEAPI_URL = "https://pokeapi.co/api/v2/generation/1";
+    const POKEAPI_URL = "https://pokeapi.co/api/v2/generation/1/";
     request(POKEAPI_URL, (err, res, body) => {
       let respuesta = JSON.parse(body).results;
     
       for (let i = 1; i <= 151; i++) {
           let Pokemon = {
-            nombre: respuesta[i].name,
+            nombre: respuesta[i].name
             // movimientos: respuesta[i].moves,
             // tipos: respuesta[i].types,
             // altura: respuesta[i].height,
